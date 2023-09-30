ca4a277c4077776f662d7b361bb1b6cbad8fa575
iamukn <n.u.kingsley@gmail.com>
2023-09-30 15:16:27 +0000

    updated the http method in password update

--- app.py
+++ app.py
@@ -15,186 +15,217 @@ try:
     db = UserInfo()
     track = Tracking()
 except Exception:
-    abort(400, 'internal error occurred')
+    abort(400, "internal error occurred")
 
 
 # handling HTTP errors
 @app.errorhandler(400)
-def BadRequest(e):    
-    """ handles 400 errors """
-    return render_template('400.html'), 400
+def BadRequest(e):
+    """handles 400 errors"""
+    return render_template("400.html"), 400
+
 
 @app.errorhandler(403)
 def forbidden(e):
-    """ handles 403 errors """
-    return render_template('403.html'), 403
+    """handles 403 errors"""
+    return render_template("403.html"), 403
+
 
 @app.errorhandler(404)
-def notFound(e):   
-    """ handles 404 errors """
-    return render_template('404.html'), 404
+def notFound(e):
+    """handles 404 errors"""
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(408)
 def request_time_out(e):
-    """ handles 408 errors """
-    return render_template('408.html'), 408
+    """handles 408 errors"""
+    return render_template("408.html"), 408
+
 
 @app.errorhandler(500)
 def internalServerError(e):
-    """ handles 500 errors """
-    return render_template('500.html'), 500
+    """handles 500 errors"""
+    return render_template("500.html"), 500
+
 
 @app.errorhandler(503)
 def service_unavailable(e):
-    """ handles 503 errors """
-    return render_template('503.html'), 503
+    """handles 503 errors"""
+    return render_template("503.html"), 503
+
 
 # root  route to serve the landing page
-@app.route('/', strict_slashes=False, methods=['GET', 'POST'])
+@app.route("/", strict_slashes=False, methods=["GET", "POST"])
 def home():
-    if request.method == 'POST':
-        track_num = request.form['tracking']
-        return redirect(url_for('tracking_', num=str(track_num.upper()) ))
+    if request.method == "POST":
+        track_num = request.form["tracking"]
+        return redirect(url_for("tracking_", num=str(track_num.upper())))
 
     """site count and landing page route """
     if g.user:
-        return render_template('index.html')
+        return render_template("index.html")
     track.site_traffic()
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login', strict_slashes=False, methods=['GET', 'POST'])
+
+@app.route("/login", strict_slashes=False, methods=["GET", "POST"])
 def login():
     """Login form page"""
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         # checks the username and password in the database
         if db.auth(username, password):
-            session['user'] = username
-            return redirect(url_for('dashboard'))
+            session["user"] = username
+            return redirect(url_for("dashboard"))
         else:
-            return render_template('login.html', username=username, password=password, err_msg='invalid username or password')
+            return render_template(
+                "login.html",
+                username=username,
+                password=password,
+                err_msg="invalid username or password",
+            )
     else:
-        return render_template('login.html')
+        return render_template("login.html")
 
 
-@app.route('/tracking', methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/tracking", methods=["GET", "POST"], strict_slashes=False)
 def tracking():
-    if request.method == 'POST':
-        tracking_num = request.form['tracking']
+    if request.method == "POST":
+        tracking_num = request.form["tracking"]
         res = track.tracker(str(tracking_num.upper()))
-        s1 = res.get('status1')
-        s2 = res.get('status2')
+        s1 = res.get("status1")
+        s2 = res.get("status2")
         if s1 and s2:
-            return render_template('tracking_info2.html', num=tracking_num, s1=s1, s2=s2)
+            return render_template(
+                "tracking_info2.html", num=tracking_num, s1=s1, s2=s2
+            )
         elif s1:
-            return render_template('tracking_info.html', num=tracking_num, s1=s1)
+            return render_template("tracking_info.html", num=tracking_num, s1=s1)
         else:
-            return redirect(url_for('tracking'))
+            return redirect(url_for("tracking"))
     else:
-        return render_template('tracking.html')
+        return render_template("tracking.html")
 
-@app.route('/tracking/<num>', methods=['GET', 'POST'], strict_slashes=False)
+
+@app.route("/tracking/<num>", methods=["GET", "POST"], strict_slashes=False)
 def tracking_(num):
     if num:
         res = track.tracker(num)
-        s1 = res.get('status1')
-        s2 = res.get('status2')
+        s1 = res.get("status1")
+        s2 = res.get("status2")
         if s1 and s2:
-            return render_template('tracking_info2.html',num=num, s1=s1, s2=s2)
+            return render_template("tracking_info2.html", num=num, s1=s1, s2=s2)
         elif s1:
-            return render_template('tracking_info.html',num=num, s1=s1)
+            return render_template("tracking_info.html", num=num, s1=s1)
         else:
-            return redirect(url_for('tracking'))
-        
+            return redirect(url_for("tracking"))
 
 
-@app.route('/signup', strict_slashes=False, methods=['GET', 'POST'])
+@app.route("/signup", strict_slashes=False, methods=["GET", "POST"])
 def signup():
     """Registration"""
-    if request.method == 'POST':
-        com_name = request.form['companyName']
-        service = request.form['service']
-        firstName = request.form['firstName']
-        email = request.form['email']
-        username = request.form['username']
-        lastName = request.form['lastName']
-        password = request.form['password']
-        companyAddr = request.form['companyAddr']
-        country = request.form['country']
-        city = request.form['city']
-        phone = request.form['phone']
+    if request.method == "POST":
+        com_name = request.form["companyName"]
+        service = request.form["service"]
+        firstName = request.form["firstName"]
+        email = request.form["email"]
+        username = request.form["username"]
+        lastName = request.form["lastName"]
+        password = request.form["password"]
+        companyAddr = request.form["companyAddr"]
+        country = request.form["country"]
+        city = request.form["city"]
+        phone = request.form["phone"]
         # Register the data to the database
         try:
-            db.register(companyName=com_name, service=service, firstName=firstName, lastName=lastName, email=email, username=username, password=password, companyAddr=companyAddr, country=country, city=city, phone=phone)
+            db.register(
+                companyName=com_name,
+                service=service,
+                firstName=firstName,
+                lastName=lastName,
+                email=email,
+                username=username,
+                password=password,
+                companyAddr=companyAddr,
+                country=country,
+                city=city,
+                phone=phone,
+            )
             email_msg(email, username)
         except Exception:
-            return 'An error occured'
+            return "An error occured"
 
         finally:
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
     else:
-        return render_template('signup.html')
+        return render_template("signup.html")
 
 
-@app.route('/dashboard', strict_slashes=False)
+@app.route("/dashboard", strict_slashes=False)
 def dashboard():
     """checks if the user is in the session and serves him his dashboard"""
     if g.user:
-        return render_template('dashboard.html', user=session['user'])
-    abort(403, 'Unauthorized')
+        return render_template("dashboard.html", user=session["user"])
+    abort(403, "Unauthorized")
+
 
 # Route to generate tracking number
-@app.route('/dashboard/generate', strict_slashes=False)
+@app.route("/dashboard/generate", strict_slashes=False)
 def generate():
     if g.user:
         """method that generates a tracking number"""
-        tracking_number = track.generate(session.get('user'))
+        tracking_number = track.generate(session.get("user"))
         return tracking_number
 
 
-@app.route('/recovery', methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/recovery", methods=["GET", "POST"], strict_slashes=False)
 def recover():
-    """ recovery password function"""
-    if request.method == 'POST':
-        rec_email = request.form['email']
+    """recovery password function"""
+    if request.method == "POST":
+        rec_email = request.form["email"]
         msg = db.recovery(rec_email)
-        return render_template('recovery.html', msg=msg)
-    return render_template('recovery.html')
+        return render_template("recovery.html", msg=msg)
+    return render_template("recovery.html")
 
-@app.route('/dashboard/reset', methods=['GET', 'POST'], strict_slashes=False)
+
+@app.route("/dashboard/reset", methods=["GET", "POST"], strict_slashes=False)
 def reset():
     """Method that changes password"""
-    if request.method == 'POST':
+    if request.method == "PUT":
         if g.user:
-            username = session['user']
-            oldPw = request.form['oldPw']
-            newPw = request.form['newPw']
+            username = session["user"]
+            oldPw = request.form["oldPw"]
+            newPw = request.form["newPw"]
             if db.pwUpdate(username, oldPw, newPw):
-                return render_template('updatePassword.htm', res='Password changed successfully')
+                return render_template(
+                    "updatePassword.htm", res="Password changed successfully"
+                )
             else:
-                return render_template('updatePassword.htm',res='Incorrect password')
-        abort(403, 'Unauthorized')
-    elif request.method == 'GET':
+                return render_template("updatePassword.htm", res="Incorrect password")
+        abort(403, "Unauthorized")
+    elif request.method == "GET":
         if g.user:
-            return render_template('updatePassword.htm')
-        abort(403, 'Unauthorized')
+            return render_template("updatePassword.htm")
+        abort(403, "Unauthorized")
 
-@app.route('/logout', strict_slashes=False)
+
+@app.route("/logout", strict_slashes=False)
 def logout():
     if g.user:
-        """ ends the session """
-        session.pop('user', None)
-        return redirect(url_for('home'))
+        """ends the session"""
+        session.pop("user", None)
+        return redirect(url_for("home"))
 
 
 # This routes executes before any requests
 @app.before_request
 def before_request():
     g.user = None
-    if 'user' in session:
-        g.user = session['user']
+    if "user" in session:
+        g.user = session["user"]
 
 
 if __name__ == "__main__":
--- models/py_files/welcome_msg.py
+++ models/py_files/welcome_msg.py
@@ -5,19 +5,24 @@
 import yagmail
 import os
 
+
 def email(email: str, user: str) -> None:
-    """ Method that sends out an email """
+    """Method that sends out an email"""
     if not email:
         return
-    
-    sender = os.getenv('email')
-    pw = os.environ.get('password')
-    #registers the email and pw to keyframes
+
+    sender = os.getenv("email")
+    pw = os.environ.get("password")
+    # registers the email and pw to keyframes
     yagmail.register(sender, pw)
     yag = yagmail.SMTP(sender)
-    headers = {'Reply-to': 'officialtrackerr@gmail.com'}
+    headers = {"Reply-to": "officialtrackerr@gmail.com"}
     # sends the mail to the client
-    with open('./models/py_files/welcome_txt.txt', 'r') as f:
+    with open("./models/py_files/welcome_txt.txt", "r") as f:
         data = f.read()
-        yag.send(to=email, subject= 'Welcome to Trackerr', headers=headers, contents=f'Hello {user} \n'+ data)
-
+        yag.send(
+            to=email,
+            subject="Welcome to Trackerr",
+            headers=headers,
+            contents=f"Hello {user} \n" + data,
+        )
