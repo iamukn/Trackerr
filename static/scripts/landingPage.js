290bcb98a07d37274b14023c8877a11b9bd2444b
Amadu <amaduhamzajaarah300@gmail.com>
2023-10-02 09:10:08 -1100

    m2nd merge

--- static/scripts/landingPage.js
+++ static/scripts/landingPage.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
     $("#searchResults").empty();
     $("#productDetails").empty();
 
-    // Perform an AJAX GET request to retrieve product data from the server
+    // Perform an AJAX GET request to retrieve parcel data from the server
     $.ajax({
       url: "your_server_endpoint_here",
-      method: "GET",
+      method: "PUT",
       dataType: "json",
       data: { searchTerm: searchTerm }, // Pass the search term to the server
       success: function (data) {
@@ -19,35 +19,69 @@ $(document).ready(function () {
         data.forEach(function (product) {
           // Display matching products within a section container
           let productHtml = `
-                      <section data-product-id="${product.id}">
-                          <h3>My Parcel</h3>
-                          <div class="product-info">
-                          <p>Tracking Number: ${product.trackingNumber}</p>
-                          <p>Status: ${product.status}</p>
-                          <button class="detail-button">View details</button>
-                      </div>
-                      </section>
-                  `;
+          <div class="product-container collapsible-container" data-product-id="${product.id}">
+          <div class="product-info">
+              
+              <p  class="item">Tracking Number: ${product.trackingNumber}</p>
+              <p class="item">Status: ${product.status}</p>
+              <button class="detail-button collapsible-button item">View details</button>
+              <div class="collapsible-content item">
+              <div class="container animate__animated animate__zoomInDown">
+              <div class="row">
+                <div class="col-12 col-md-10 hh-grayBox pt45 pb20">
+                  <div class="row justify-content-between">
+                    <div class="order-tracking completed">
+                      <span class="is-complete"></span>
+                      <p id="#shippedStatus">Shipped<br /><span id="#updateDate">Mon, June 24</span></p>
+                    </div>
+                    <div class="order-tracking completed">
+                      <span class="is-complete"></span>
+                      <p id="inTransitStatus">In Transit<br /><Tue, id="#updateDate">Tue, June 25</span></p>
+                    </div>
+                    <div class="order-tracking">
+                      <span class="is-complete"></span>
+                      <p id="deliveredStatus">Delivered<br /><span id="#updateDate">Fri, June 28</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+              </div>
+              
+            </div>
+           
+              
+            </div>
+          `;
           $("#searchResults").append(productHtml);
         });
 
-        //This script will redirect the user to the tracking status page
-        $("#detail-button").click(function () {
-          const trackingNumber = $("#searchInput").val().toLowerCase();
-
-          // Redirect to the tracking status page with the tracking number as a URL parameter
-          window.location.href = `./trackingStatus.html?trackingNumber=${trackingNumber}`;
-        });
-
-        // Add click event to "Track" buttons for more details
-        $(".view-details").click(function () {
-          // Get the product ID from the parent section's data attribute
-          let productId = $(this).closest("section").data("product-id");
+        // Add click event to make "Show Details" buttons collapsible
+        $(".collapsible-button").click(function () {
+          $(this).next(".collapsible-content").slideToggle();
         });
       },
       error: function (error) {
         console.error("Error fetching data:", error);
       },
     });
+  });
+  // Event listener for the "Track" button
+  $("#trackButton").click(function () {
+    let searchTerm = $("#searchInput").val().trim().toLowerCase();
+
+    if (searchTerm === "") {
+      // Display a message below the input field to prompt the user to enter a tracking number
+      $("#searchInput").addClass("invalid");
+      $("#searchInput").attr("placeholder", "Please enter a tracking number");
+      return;
+    }
+
+    // Clear any previous error messages
+    $("#searchInput").removeClass("invalid");
+    $("#searchInput").attr("placeholder", "Enter your tracking number");
+
+    // Call the function to perform the product search
+    performProductSearch(searchTerm);
   });
 });
